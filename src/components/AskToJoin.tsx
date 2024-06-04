b8875753931d1c3673c9bba9cb02639bfba0807b
Anagha Sarmalkar <anaghasarmalkar@gmail.com>
2024-06-04 14:26:20 -0600

    Modal to ask to join a room.

---
+++ src/components/AskToJoin.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { CurrentWindowType } from "@/app/page";
+
+type AskToJoinProps = {
+  websocket: WebSocket | null;
+  userUUID: string;
+  roomUUID: string;
+  handleCurrentWindowState: (windowState: CurrentWindowType) => void;
+  handleSetCurrentRoom: (roomUUID: string) => void;
+};
+export default function AskToJoin({
+  websocket,
+  userUUID,
+  roomUUID,
+  handleCurrentWindowState,
+  handleSetCurrentRoom,
+}: AskToJoinProps) {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleJoin = () => {
+    if (websocket !== null) {
+      const joinEvent = {
+        entity: "room",
+        action: "join",
+        username: userUUID,
+        entity_data: {
+          room_uuid: roomUUID,
+        },
+      };
+      websocket.send(JSON.stringify(joinEvent));
+    }
+  };
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle>Join Room</DialogTitle>
+      <DialogContent>
+        <DialogContentText>Do you want to join this room?</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          variant="outlined"
+          onClick={() => {
+            handleClose();
+            handleCurrentWindowState(CurrentWindowType.Welcome);
+          }}
+        >
+          Cancel
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={() => {
+            handleJoin();
+            handleSetCurrentRoom(roomUUID);
+            handleCurrentWindowState(CurrentWindowType.JoinedRoom);
+          }}
+        >
+          Join
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
