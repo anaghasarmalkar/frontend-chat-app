a86c290f98c6742c7ffd5dfcae252b326afdc9ee
Anagha Sarmalkar <anaghasarmalkar@gmail.com>
2024-06-08 02:09:01 -0600

    Added a bunch of functions in useCallback. Storing entire room data for current room instead of just uuid.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useState, useCallback } from "react";
+import { useMemo, useState, useCallback } from "react";
 import { useWebSocket } from "@/hooks/useWebSocket";
 import { v4 as uuidv4 } from "uuid";
 import {
@@ -23,12 +23,13 @@ import { CurrentSectionType, Message, Room } from "@/utilities/types";
 import ChatRoom from "@/components/ChatRoom";
 import AskToJoin from "@/components/AskToJoin";
 import { useReceivedData } from "@/hooks/useReceivedData";
-
+import GroupRemoveIcon from "@mui/icons-material/GroupRemove";
+const drawerWidth = 250;
 export default function Home() {
   const [requestedSection, setRequestedSection] = useState<CurrentSectionType>(
     CurrentSectionType.Welcome
   );
-  const [currentRoom, setCurrentRoom] = useState<string>("");
+  const [currentRoom, setCurrentRoom] = useState<Room | null>(null);
 
   const userUUID = useMemo(() => uuidv4(), []);
   const initEvent = useMemo(() => {
@@ -47,43 +48,87 @@ export default function Home() {
   const { availableRooms, joinedRooms, roomChat } =
     useReceivedData(receivedData);
 
-  const drawerWidth = 250;
+  const handleSetCurrentRoom = useCallback(
+    (room: Room) => setCurrentRoom(room),
+    []
+  );
+
+  const handleClearRoom = useCallback(() => setCurrentRoom(null), []);
+
+  const handleJoinedRoomClick = useCallback((room: Room) => {
+    setRequestedSection(CurrentSectionType.JoinedRoom);
+    setCurrentRoom(room);
+  }, []);
+
+  const handleAvailableRoomClick = useCallback((room: Room) => {
+    setRequestedSection(CurrentSectionType.AskToJoinRoom);
+    setCurrentRoom(room);
+  }, []);
 
   // Its annoying to separate component and send data as props. too many props
   const ResultComponent = useCallback(() => {
+    let goToDefault = false;
+    const defaultPage = <Typography paragraph>Welcome page</Typography>;
+
     switch (requestedSection) {
       case CurrentSectionType.NewRoom:
         return <NewRoom websocket={websocket} userUUID={userUUID} />;
       case CurrentSectionType.JoinedRoom:
-        return (
-          <ChatRoom
-            websocket={websocket}
-            userUUID={userUUID}
-            roomUUID={currentRoom}
-            messages={roomChat?.get(currentRoom) || new Set<Message>()}
-          />
-        );
+        if (currentRoom) {
+          return (
+            <ChatRoom
+              websocket={websocket}
+              userUUID={userUUID}
+              roomUUID={currentRoom.room_uuid}
+              messages={
+                roomChat?.get(currentRoom.room_uuid) || new Set<Message>()
+              }
+            />
+          );
+        } else {
+          goToDefault = true;
+          break;
+        }
+
       case CurrentSectionType.AskToJoinRoom:
-        return (
-          <AskToJoin
-            websocket={websocket}
-            userUUID={userUUID}
-            roomUUID={currentRoom}
-            handleCurrentWindowState={(windowState: CurrentSectionType) =>
-              setRequestedSection(windowState)
-            }
-            handleSetCurrentRoom={(roomUUID: string) =>
-              setCurrentRoom(roomUUID)
-            }
-          />
-        );
+        if (currentRoom) {
+          return (
+            <AskToJoin
+              websocket={websocket}
+              userUUID={userUUID}
+              room={currentRoom}
+              handleCurrentWindowState={(windowState: CurrentSectionType) =>
+                setRequestedSection(windowState)
+              }
+              handleSetCurrentRoom={handleSetCurrentRoom}
+              handleClearRoom={handleClearRoom}
+            />
+          );
+        } else {
+          goToDefault = true;
+          break;
+        }
+
       case CurrentSectionType.Welcome:
         return <Typography paragraph>Welcome page</Typography>;
       default:
-        return <Typography paragraph>Welcome page</Typography>;
+        return defaultPage;
     }
-  }, [currentRoom, requestedSection, roomChat, userUUID, websocket]);
 
+    if (goToDefault) {
+      return defaultPage;
+    }
+  }, [
+    currentRoom,
+    handleClearRoom,
+    handleSetCurrentRoom,
+    requestedSection,
+    roomChat,
+    userUUID,
+    websocket,
+  ]);
+
+  console.log(currentRoom);
   return (
     <Box display="flex" flexDirection="column" width="100%" height="100vh">
       <CssBaseline />
@@ -101,13 +146,34 @@ export default function Home() {
               </IconButton>
             </Tooltip>
           </Header>
+          <Divider />
         </Box>
         <Divider orientation="vertical" />
-        <Box flexGrow="1">
-          <Header title="Room Name and Details"></Header>
-        </Box>
+        {currentRoom && (
+          <Box flexGrow="1">
+            {/* Default can be last accessed room/latest chat */}
+            <Header
+              title={currentRoom.name}
+              description={currentRoom.description}
+            >
+              <Tooltip title="Leave group" arrow>
+                <IconButton
+                  aria-label="leave-group"
+                  onClick={() => {
+                    // send message "leave" to websocket
+                    // Remove from joined rooms list / or that will update when server sends response for leave
+                    handleClearRoom();
+                  }}
+                >
+                  <GroupRemoveIcon />
+                </IconButton>
+              </Tooltip>
+            </Header>
+            <Divider />
+          </Box>
+        )}
       </Box>
-      <Divider />
+
       <Box
         display="flex"
         flexDirection="row"
@@ -139,19 +205,13 @@ export default function Home() {
             <RoomsList
               title={"Joined Rooms"}
               roomsList={joinedRooms}
-              handleRoomClick={(uuid: string) => {
-                setRequestedSection(CurrentSectionType.JoinedRoom);
-                setCurrentRoom(uuid);
-              }}
+              handleRoomClick={handleJoinedRoomClick}
             />
             <Divider />
             <RoomsList
               title={"Available Rooms"}
               roomsList={availableRooms}
-              handleRoomClick={(uuid: string) => {
-                setRequestedSection(CurrentSectionType.AskToJoinRoom);
-                setCurrentRoom(uuid);
-              }}
+              handleRoomClick={handleAvailableRoomClick}
             />
             <Divider />
           </Box>
