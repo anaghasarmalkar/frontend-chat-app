36a0bd49eea468ce86ddc9743f586bff19145e0a
Anagha Sarmalkar <anaghasarmalkar@gmail.com>
2024-07-12 02:56:51 -0600

    Update useReceivedData hook to get data from backend.

--- src/hooks/useReceivedData.ts
+++ src/hooks/useReceivedData.ts
@@ -1,56 +1,40 @@
 import { Message, Room } from "@/utilities/types";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-export function useReceivedData(receivedData: any) {
+export function useReceivedData(token: string) {
   const [availableRooms, setAvailableRooms] = useState<Room[]>([]);
   const [joinedRooms, setJoinedRooms] = useState<Room[]>([]);
-  const [roomChat, setRoomChat] = useState<Map<string, Set<Message>>>();
+  const [createdRooms, setCreatedRooms] = useState<Room[]>([]);
+  // const [roomChat, setRoomChat] = useState<Map<string, Set<Message>>>();
+
+  const getRooms = useCallback(async () => {
+    try {
+      const response = await fetch("http://0.0.0.0:8081/api/rooms", {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      switch (response.status) {
+        case 200:
+          const data = await response.json();
+          setAvailableRooms(data.data.available);
+          setJoinedRooms(data.data.joined);
+          setCreatedRooms(data.data.created);
+          break;
+        default:
+          break;
+      }
+    } catch (error) {
+      // How to show an alert when POST fails? Because browser makes OPTIONS request and that fails so POST is never called and we dont reach here
+      console.error(error);
+    }
+  }, [token]);
 
   useEffect(() => {
-    if (
-      receivedData &&
-      receivedData.entity === "server" &&
-      receivedData.action === "init" &&
-      receivedData.result === "success"
-    ) {
-      console.log("Connected to server.");
-    }
-    if (
-      receivedData &&
-      receivedData.entity === "room" &&
-      receivedData.result === "success"
-    ) {
-      if (receivedData.entity_data !== null) {
-        if (!!receivedData.entity_data.joined) {
-          setJoinedRooms(receivedData.entity_data.joined);
-        }
-        if (!!receivedData.entity_data.available) {
-          setAvailableRooms(receivedData.entity_data.available);
-        }
-      }
-      if (
-        receivedData.action === "message" &&
-        receivedData.result === "success"
-      ) {
-        if (receivedData.entity_data !== null) {
-          const { room_uuid, message } = receivedData.entity_data;
-          setRoomChat((prev) => {
-            const newMap = new Map(prev);
-            if (!newMap.has(room_uuid)) {
-              newMap.set(room_uuid, new Set<Message>());
-            }
-            newMap.get(room_uuid)?.add(message);
-            return newMap;
-          });
-        }
-      }
-    }
-  }, [
-    receivedData,
-    receivedData?.entity,
-    receivedData?.action,
-    receivedData?.result,
-    receivedData?.entity_data,
-  ]);
-  return { availableRooms, joinedRooms, roomChat };
+    getRooms();
+  }, [getRooms]);
+  return { availableRooms, joinedRooms, createdRooms };
 }
